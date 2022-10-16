f1a8523efaa66c2286c526e7f189bf39787ffa92
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2022-10-16 14:45:26 -0600

    added missing file header

--- src/Interfaces/IHandleDtmfTimeout.cs
+++ src/Interfaces/IHandleDtmfTimeout.cs
@@ -1,4 +1,7 @@
-﻿namespace CallAutomation.Extensions.Interfaces;
+﻿// Copyright (c) 2022 Jason Shave. All rights reserved.
+// Licensed under the MIT License.
+
+namespace CallAutomation.Extensions.Interfaces;
 
 public interface IHandleDtmfTimeout
 {
