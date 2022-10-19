2c8a5becc78acfd48ac08ab6927dc591fa0c769c
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2022-10-19 09:06:32 -0600

    added override

---
+++ src/Models/CallAutomationSdkClientOptions.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) 2022 Jason Shave. All rights reserved.
+// Licensed under the MIT License.
+
+namespace CallAutomation.Extensions.Models;
+
+public sealed class CallAutomationSdkClientOptions
+{
+    public string ConnectionString { get; set; } = string.Empty;
+
+    public Uri? OverrideEndpointUri { get; set; }
+}
